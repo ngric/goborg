d988c1fb054eb34958c553e36b23e2a2b8298ea7
nile <nile@email.arizona.edu>
2019-12-09 02:01:36 -0700

    comments

--- bot.go
+++ bot.go
@@ -1,3 +1,18 @@
+/*
+* Assignment:	Final Project, Part #3
+* Author:		Nile Grice (nile@email.arizona.edu)
+*
+* Course:		CSC372
+* Instructor:	L. McCann
+* TA:			Tito Ferra
+* Due Date:		December 9, 2019
+*
+* Description:	A markov-chain based discord chatbot
+*		Usage: goborg -t <discord api token>
+*
+* History URL:	https://ngric.github.io/goborg/
+ */
+
 package main
 
 import (
@@ -6,6 +21,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"os/signal"
 	"strings"
@@ -18,52 +34,39 @@ import (
 var (
 	Chain    markov.Chain
 	token    string
-	lastChan string
+	lastChan string  // store this so that we can give a parting message
+	rate     float32 // probability that any given message will get a reply
 )
 
+// runs at program launch. reads in token from arguments and loads
+// the bot's "brain" from disk
 func init() {
 	flag.StringVar(&token, "t", "", "Bot Token")
 	flag.Parse()
 	load()
-}
-
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-
-	sarr := strings.Fields(m.Content)
-
-	if len(sarr) > 0 {
-		for i, v := range sarr {
-			if i == len(sarr)-1 {
-				go Chain.AddEdge(v, "")
-			} else {
-				go Chain.AddEdge(v, sarr[i+1])
-			}
-		}
-		reply := Chain.GetLine(sarr[0])
-		lastChan = m.ChannelID
-		s.ChannelMessageSend(m.ChannelID, reply)
-	}
-
+	rate = .8
 }
 
 func main() {
+	// instantiate a discord session with passed token
 	discord, err := discordgo.New("Bot " + token)
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
 		return
 	}
 
+	// register messageCreate method as handler for message events
 	discord.AddHandler(messageCreate)
 
+	// open connection to discord
 	err = discord.Open()
 	if err != nil {
 		fmt.Println("error opening connection,", err)
 		return
 	}
 
+	// continue to run until we receive an interrupt of some variety
+	// eg: Ctrl-C
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
@@ -73,9 +76,12 @@ func main() {
 	save()
 }
 
+// loads a Chain struct (the bot's 'brain') from disk.
+// expects to find it in a file named 'brain', if it can't find or open
+// such a file, the bot will start from empty
 func load() {
-	f, err := os.Open("brain")
-	defer f.Close()
+	f, err := os.Open("brain") // attempt to open file
+	defer f.Close()            // close file when this method returns
 
 	if err != nil {
 		fmt.Println("Unable to open brain. Creating new...")
@@ -83,8 +89,9 @@ func load() {
 		return
 	}
 
-	r := bufio.NewReader(f)
-	dec := gob.NewDecoder(r)
+	// read opened file from disk
+	r := bufio.NewReader(f)  // file stream
+	dec := gob.NewDecoder(r) // stream decoder
 	err = dec.Decode(&Chain)
 
 	if err != nil {
@@ -92,22 +99,59 @@ func load() {
 	}
 }
 
+// saves a Chain struct (the bot's 'brain') to disk.
+// will overwrite the file if it already exists
 func save() {
-	f, err := os.Create("brain")
-	defer f.Close()
+	f, err := os.Create("brain") // truncates the file if it already exists
+	defer f.Close()              // make sure the file is closed when this method returns
 
 	if err != nil {
 		fmt.Println("Unable to open brain for saving.")
 		return
 	}
 
-	w := bufio.NewWriter(f)
-	enc := gob.NewEncoder(w)
+	// write Chain to disk
+	w := bufio.NewWriter(f)  // file output stream
+	enc := gob.NewEncoder(w) // encodes whatever it's given, then passes it to w
 	err = enc.Encode(&Chain)
 
 	if err != nil {
 		log.Fatal("Error while writing brain, ", err)
 	}
 
-	w.Flush()
+	w.Flush() // flush output buffers to disk
+}
+
+// method that runs every time a message is received from discord
+// trains the markov chain on the contents of the message, and potentially
+// gets/sends a reply
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// ignore messages that originate from goborg itself
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	// break message into string array delimited on spaces
+	sarr := strings.Fields(m.Content)
+
+	if len(sarr) > 0 { // messages should always be non-empty, but just in case
+		for i, v := range sarr {
+			// last word in message has an edge to "", which
+			// we're using for termination
+			if i == len(sarr)-1 {
+				go Chain.AddEdge(v, "")
+			} else { // add edge between current and following words
+				go Chain.AddEdge(v, sarr[i+1])
+			}
+		}
+
+		// roll for a reply
+		if rand.Float32() < rate {
+			reply := Chain.GetLine(sarr[0])
+			s.ChannelMessageSend(m.ChannelID, reply)
+		}
+
+		lastChan = m.ChannelID // store for "goodbye" message on quit
+	}
+
 }
--- markov/chain.go
+++ markov/chain.go
@@ -1,3 +1,17 @@
+/*
+* Assignment:	Final Project, Part #3
+* Author:		Nile Grice (nile@email.arizona.edu)
+*
+* Course:		CSC372
+* Instructor:	L. McCann
+* TA:			Tito Ferra
+* Due Date:		December 9, 2019
+*
+* Description:	A markov-chain implementation
+*
+* History URL:	https://ngric.github.io/goborg/
+ */
+
 package markov
 
 import (
@@ -5,32 +19,43 @@ import (
 	"sync"
 )
 
+// Overall markov-chain struct. Just a string:Word map with a mutex
 type Chain struct {
 	mut   sync.RWMutex
 	Words map[string]Word
 }
 
+// Word struct contains a string:int map representing edges from this word
+// to other words, and thier weights. Links keeps track of the total weight
+// of all edges out of this Word.
 type Word struct {
 	Edges map[string]int
 	Links int
 }
 
+// instantiates a new Word
 func newWord() Word {
 	var w Word
 	w.Edges = make(map[string]int)
 	return w
 }
 
+// instantiates a new Chain
 func NewChain() Chain {
 	var c Chain
 	c.Words = make(map[string]Word)
 	return c
 }
 
+// adds a new directed edge between two words. If a matching edge already exists
+// the weight of the edge is incremented by one.
 func (c *Chain) AddEdge(from, to string) {
+	// potentially excessive mutex. Invalidates any potintial Chain-related
+	// concurrency as currently implemented
 	c.mut.Lock()
 	defer c.mut.Unlock()
 
+	// see if "from" is a word that exists in the chain
 	w, ok := (*c).Words[from]
 
 	if ok { // if the starting word exists, add edge leading to dest word
@@ -51,12 +76,28 @@ func (c *Chain) AddEdge(from, to string) {
 	}
 }
 
-func (c *Chain) nextFrom(s string) string {
-	c.mut.RLock()
-	defer c.mut.RUnlock()
-	w := (*c).Words[s]
+// Uses the Chain to generate a "sentence."
+// generation is seeded on the passed string
+func (c *Chain) GetLine(start string) string {
+	s, w := "", c.nextFrom(start)
+	for w != "" { // continue building the string until we get an empty word
+		s += w + " "
+		w = c.nextFrom(w)
+	}
+	return s
+}
 
-	if l := w.Links; l > 0 {
+// retreives a word adjacent from passed string in the Chain.
+// retreived word is influenced by weight of the edge connecting it
+func (c *Chain) nextFrom(s string) string {
+	c.mut.RLock() // lock chain
+	defer c.mut.RUnlock()
+	w := (*c).Words[s] // assume s exists in the chain, get its neighbors
+
+	if l := w.Links; l > 0 { // there should be no words with 0 links, but this
+		// may change
+		// choose next word by rolling a random number n, and summing weights
+		// until they surpass n
 		n := rand.Intn(l)
 		sum := 0
 		for k, v := range w.Edges {
@@ -66,14 +107,6 @@ func (c *Chain) nextFrom(s string) string {
 			}
 		}
 	}
-	return ""
-}
 
-func (c *Chain) GetLine(start string) string {
-	s, w := "", c.nextFrom(start)
-	for w != "" {
-		s += w + " "
-		w = c.nextFrom(w)
-	}
-	return s
+	return ""
 }
